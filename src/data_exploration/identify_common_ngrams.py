de4008423f512c27a09ad940ea9e9da8170dab58
matthewheston <matthewheston@gmail.com>
2014-06-09 14:11:50 -0500

    Create README file for exploration scripts to document usage.

--- src/data_exploration/identify_common_ngrams.py
+++ src/data_exploration/identify_common_ngrams.py
@@ -1,12 +1,3 @@
-# I wrote this with the intention of trying to extract meaningful n-grams
-# from the excerpts columns of the earmarks listed at the website
-# http://earmarks.omb.gov/. Specifically from the CSV files you can download
-# from i.e., http://earmarks.omb.gov/earmarks-public/2010_download.html.
-# Initial results don't seem particularly insightful, but I've only looked at
-# one year, and I'm not sure the measures I'm using are the best. The goal
-# for this was to try to see if there are common phrases in these
-# excerpts that identify them as earmarks.
-
 import nltk
 from nltk.collocations import *
 import pandas as pd
