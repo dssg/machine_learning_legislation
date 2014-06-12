d30761cf182e34f6d56e81019edd058778700576
matthewheston <matthewheston@gmail.com>
2014-06-12 14:15:19 -0500

    Run bigrams and trigrams in ngram script.

--- src/python/data_exploration/identify_common_ngrams.py
+++ src/python/data_exploration/identify_common_ngrams.py
@@ -7,8 +7,9 @@ parser = argparse.ArgumentParser(description='Evaluate earmarks csv.')
 parser.add_argument('--file', action='store', dest='file_location')
 parser.add_argument('--measure', action='store', dest='measure_type',
         help='bigram measure to use in rankings.')
-parser.add_argument('--freq', action='store', type=int, dest='freq_filter',
-        help='minimum number of times n-gram must appear to be considered.')
+parser.add_argument('--freq', action='store', type=int, dest='freq_filter')
+parser.add_argument('--n', action='store', type=int, dest='ngram',
+        help='use bigrams or trigrams.')
 results = parser.parse_args()
 
 # read in the csv and get the excerpts column
@@ -20,13 +21,19 @@ tokens = nltk.wordpunct_tokenize(excerpts_text)
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 trigram_measures = nltk.collocations.TrigramAssocMeasures()
 
-finder = BigramCollocationFinder.from_words(tokens)
+if results.ngram == 2:
+    finder = BigramCollocationFinder.from_words(tokens)
+else:
+    finder = TrigramCollocationFinder.from_words(tokens)
 
 # only bigrams that appear 5+ times
 finder.apply_freq_filter(results.freq_filter)
 
-# return the 10 n-grams with the highest PMI
 # TODO: not sure this use of eval makes sense, but works
-highest_pmi_bigrams = finder.nbest(eval('bigram_measures.' + results.measure_type), 10)
+if results.ngram == 2:
+    collocs = finder.nbest(eval('bigram_measures.' + results.measure_type), 10)
+    finder = BigramCollocationFinder.from_words(tokens)
+else:
+    collocs = finder.nbest(eval('trigram_measures.' + results.measure_type), 10)
 
-print highest_pmi_bigrams
+print collocs
