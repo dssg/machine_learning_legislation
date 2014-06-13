1f874c91ef86f07ee6ac677f3fae3a0918b2beb5
Madian Khabsa <madian@psu.edu>
2014-06-13 15:11:10 +0000

    add shell script to call congress report downloaded using gnu parallel

---
+++ sh/run_fetch_congress_reports.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# usage: ./run_fetch_congress_reports.sh <report_ids_file> <num_threads> <out_dir> 
+HOME="/home/mkhabsa/code/machine_learning_legislation"
+CMD="python $HOME/src/python/bill_fetcher/congress_report_downloader.py"
+FILE_NAMES=$1
+THREADS=$2
+OUT_DIR=$3
+
+cat $FILE_NAMES | parallel --tmpdir /tmp/congress-reports/ --files -P $THREADS $CMD --fetch "{}" --outdir $OUT_DIR
