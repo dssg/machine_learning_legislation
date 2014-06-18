116ce6ad33e6e1f1aa8d5b00ffc2550ccf0e4e17
matthewheston <matthewheston@gmail.com>
2014-06-18 14:58:46 -0500

    Update bill association script to write file paths.

--- src/python/bill_fetcher/associate_reports_bills.py
+++ src/python/bill_fetcher/associate_reports_bills.py
@@ -4,7 +4,6 @@ import re
 from bs4 import BeautifulSoup
 
 root_dir = "/mnt/data/sunlight/congress_reports/"
-conn = psycopg2.connect("dbname=harrislight user=harrislight password=harrislight host=dssgsummer2014postgres.c5faqozfo86k.us-west-2.rds.amazonaws.com")
 
 # loop through each folder
 def get_reports(path, congress):
@@ -14,10 +13,10 @@ def get_reports(path, congress):
         else:
             xml_file = os.path.join(path, report, "mods.xml")
             associated_bill = get_associated_bill(xml_file)
-            bill_dir = os.listdir(os.path.join(path, report))
+            report_dir = os.listdir(os.path.join(path, report))
             if "mods.xml" in bill_dir:
-                bill_dir.remove("mods.xml")
-            write_bill_association(conn, bill_dir[0], clean_up_bill_name(associated_bill, congress))
+                report_dir.remove("mods.xml")
+            write_bill_association(os.path.join(path, report, report_dir), get_bill_directory(associated_bill, congress))
 
 def get_associated_bill(xml_file):
     bill_name = ""
@@ -29,28 +28,46 @@ def get_associated_bill(xml_file):
         bill_name = bill.text
     return bill_name
 
-def write_bill_association(conn, report_id, bill_id):
-    cmd = "INSERT INTO reports_bills (report_id, bill_id) values (%s, %s)"
-    params = (report_id, bill_id)
-    cur = conn.cursor()
-    cur.execute(cmd, params)
-    conn.commit()
+def write_bill_association (report_id, bill_id):
+    print "%s,%s" % (report_id, bill_id)
 
-def clean_up_bill_name(bill_name, congress_number):
+def get_bill_directory(bill_name, congress_number):
     if not bill_name:
         return ""
-    clean_name = ""
-    # house or senate identifier
-    if bill_name[0].lower() == 's':
-        clean_name = clean_name + "s"
-    if bill_name[0].lower() == 'h':
-        clean_name = clean_name + "hr"
-    # bill number
-    bill_number= re.compile("\d+")
-    clean_name = clean_name + bill_number.search(bill_name).group()
 
-    clean_name = clean_name + "-%s" % congress_number
-    return clean_name
+    bill_dir = ""
+    root_bill_dir = "/mnt/data/sunlight/bills/"
+    bill_dir = os.path.join(root_bill_dir, congress_number)
+    bill_dir = os.path.join(bill_dir, "bills")
+    bill_dir = os.path.join(bill_dir, get_bill_type_dir(bill_name))
+    bill_dir = os.path.join(bill_dir, get_full_bill_dir(bill_name))
+
+def get_bill_type_dir(bill_name):
+    if "H. Con. Res." in bill_name:
+        return "hconres"
+    if "H.J. Res." in bill_name:
+        return "hjres"
+    if "H.R." in bill_name:
+        return "hr"
+    if "H. Res." in bill_name:
+        return "hres"
+    if "S. Con. Res." in bill_name:
+        return "sconres"
+    if "S.J. Res." in bill_name:
+        return "sjres"
+    if "S. Res." in bill_name:
+        return "sres"
+
+    return "s"
+
+def get_full_bill_dir(bill_name):
+    bill_type = get_bill_type(bill_name)
+
+    bill_num_regex = re.compile(r"\d+")
+    number_match = bill_num_regex.search(bill_name)
+    bill_number = number.match.group()
+
+    return bill_type + bill_number
 
 def main():
     # for each congress
