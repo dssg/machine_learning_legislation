23d7863845d8b385e10f3a964c5a534149c57670
matthewheston <matthewheston@gmail.com>
2014-06-19 11:39:32 -0500

    Start working on table identifier code.

---
+++ src/python/util/text_table_tools.py
@@ -0,0 +1,24 @@
+import re
+
+def paragraphs(fileobj, separator='\n'):
+    if separator[-1:] != '\n': separator += '\n'
+    paragraph = []
+    for line in fileobj:
+        if line == separator:
+            if paragraph:
+                yield ''.join(paragraph)
+                paragraph = []
+        else:
+            paragraph.append(line)
+    if paragraph: yield ''.join(paragraph)
+
+def is_table(paragraph):
+    dots_re = re.compile(r"\.\.\.")
+    digit_re = re.compile(r"\d+")
+    lines = paragraph.split("\n")
+    matching_lines = [line for line in lines if dots_re.search(line) and digit_re.search(line)]
+
+    if float(len(matching_lines))/len(lines) > 0.5:
+        return True
+    else:
+        return False
