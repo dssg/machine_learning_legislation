72b855d1b713836b4d14eebfa0066d5eabdfbc82
Madian Khabsa <madian@psu.edu>
2014-06-19 23:28:55 -0500

    edited bills_ner.py to support new database, and added path2db_id in path_tools.py and vice versa function

--- src/python/ner/bills_ner.py
+++ src/python/ner/bills_ner.py
@@ -1,10 +1,12 @@
-import os, sys
+import os, sys, inspect
+sys.path.insert(0, os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],".."))))
 from calais import Calais
 import codecs
 import random
 import psycopg2
+from util import path_tools
 
-USAGE = "python %s <bill-version-file>"
+USAGE = "python %s <bill-version-file> <bill|report>"
 API_KEYS = ["wdbkpbpsksskkbm2rpqfm4xa", "mt5qu3e4jdrd6jpc9r9ecama",
 "k9fb7rfh7hpbfp238whuggrr","55rared7un2pnjr23kjtctes"]
 MAX_TEXT_LENGTH = 100000
@@ -36,10 +38,14 @@ def extract_entities(text, offset=0):
         for calais_entity in result.entities:
             e_type = calais_entity['_type']
             e_name = calais_entity['name']
+            e_url = None
+            if calais_entity.has_key('resolutions'):
+                e_url=calais_entity['resolutions']['id']
             for instance in calais_entity['instances']:
                 e = Entity()
                 e.name = e_name
                 e.type = e_type
+                e.url = e_url
                 e.offset = offset + instance['offset']
                 e.length = instance['length']
                 e.text = instance['exact']
@@ -51,15 +57,24 @@ def extract_entities(text, offset=0):
         return []
     
     
-def handle_file(bill_id, content):
-    clean_content = content[17:]
-    offset = 17
+def handle_file(document_path, content, doc_type):
+    """
+    doc_type: bill or report
+    """
+    clean_content = content
+    offset = 0
+    if doc_type == 'report':
+        text_mark = "<body><pre>"
+        i = clean_content.find(text_mark)
+        if i > 0:
+            clean_content = clean_content[i+len(text_mark):]
+            offset = i + len(text_mark)
     chunks = chunk_file(clean_content)
     entities = []
     for chunk in chunks:
         entities = entities + extract_entities(chunk, offset)
         offset += len(chunk)
-    insert_entities_to_db(entities, bill_id)
+    insert_entities_to_db(entities, document_path, doc_type)
     
         
     
@@ -87,11 +102,16 @@ def get_chunk(text, limit):
     else:
         return chunk[:chunk.rfind('\n') + 1]
     
-def insert_entities_to_db(entities, bill_id):
+def insert_entities_to_db(entities, document_path, doc_type):
     conn = psycopg2.connect(CONN_STRING)
-    cmd = "insert into billentities (entity_text, entity_type, entity_offset, \
-    entity_length, entity_name, bill_id) values (%s, %s, %s, %s, %s, %s)"
-    params = [(e.text, e.type, e.offset, e.length, e.name, bill_id) for e in entities]
+    cmd = "insert into entities (entity_text, entity_type, entity_offset, \
+    entity_length, entity_inferred_name, source,  document_id, entity_url) values (%s, %s, %s, %s, %s,'calais',%s, %s)"
+    if doc_type =='senate':
+        obj = path_tools.BillPathUtils(document_path)
+    else:
+        obj = path_tools.ReportPathUtils(document_path)
+    doc_id = obj.get_db_document_id() 
+    params = [(e.text, e.type, e.offset, e.length, e.name, doc_id, e.url) for e in entities]
     cur = conn.cursor()
     cur.executemany(cmd, params)
     conn.commit()
@@ -99,11 +119,12 @@ def insert_entities_to_db(entities, bill_id):
     
 def main():
     content = read_file(sys.argv[1])
-    bill_id = sys.argv[1].split('/')[-1]
-    handle_file(bill_id,content)
+    document_path = sys.argv[1]
+    doc_type = sys.argv[2]
+    handle_file(document_path,content, doc_type)
     
 if __name__=="__main__":
-    if len(sys.argv)<2:
+    if len(sys.argv)<3:
         print USAGE
     else:
         main()
