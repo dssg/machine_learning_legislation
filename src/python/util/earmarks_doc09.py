641749d899c19c8127cd37e32690213720f19e74
OpsWorks user vvora <vvora@sunlight.localdomain>
2014-06-25 16:40:01 +0000

    earmarks09 changed

--- src/python/util/earmarks_doc09.py
+++ src/python/util/earmarks_doc09.py
@@ -10,6 +10,69 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 print sys.getdefaultencoding()
 
+## processing the omb file ## 
+#original = pd.read_csv("2010_app.csv",low_memory=False)
+def csvTopandas(earmarks):
+    earmark_ids = earmarks['earmark_id'].tolist()
+    com_names = ['hc_f_cttn_reference_name','hf_f_cttn_reference_name','sc_f_cttn_reference_name','sf_f_cttn_reference_name', 'c_f_cttn_reference_name']
+    meta_data = {'hc_f_cttn_reference_name':['hc_f_cttn_location','hc_f_cttn_excerpt','hc_earmarks_total'],
+             'hf_f_cttn_reference_name':['hf_f_cttn_location','hf_f_cttn_excerpt','hf_earmarks_total'],
+             'sc_f_cttn_reference_name': ['sc_f_cttn_location','sc_f_cttn_excerpt','sc_earmarks_total'],
+             'sf_f_cttn_reference_name': ['sf_f_cttn_location','sf_f_cttn_excerpt','sf_earmarks_total'],
+             'c_f_cttn_reference_name' : ['c_f_cttn_location','c_f_cttn_excerpt','c_earmarks_total']
+             }
+    em_df = []
+    rows = earmarks.shape[0]
+    for i in range(rows): 
+        for name in com_names:
+            if not pd.isnull(earmarks.ix[i,name]):
+                em_df.append([earmarks.ix[i,'earmark_id'],earmarks.ix[i,name],earmarks.ix[i,meta_data[name][0]],earmarks.ix[i,meta_data[name][1]],earmarks.ix[i,meta_data[name][2]]])
+    
+    em_dframe = pd.DataFrame(columns = ['earmark_id','doc_name','page_location','excerpt','earmarks_total'],index=np.arange(len(em_df)))
+    
+    for value in range(len(em_df)):
+        em_dframe.ix[value] = em_df[value]
+    
+    grouped = em_dframe.groupby(['earmark_id','doc_name'])
+    
+    index = [group_values[0] for group_values in grouped.groups.values()]
+    
+    unique_grouped = em_dframe.reindex(index)
+    
+    return unique_grouped 
+
+#earmarks_2009 = pd.read_csv("2009_app.csv",low_memory=False)
+unique_grouped09 = csvTopandas(original)
+unique_grouped09['path'] = ''
+for i in unique_grouped09.iterrows():
+    value = list(i[1])[1]
+    if len(re.findall('\d+',value))>0:
+        poc = value[0:value.index(re.findall('\d+',value)[0])]
+        
+        if 'Div. ' in poc:
+            if 'Defense' in value or 'Homeland Security' in value or 'Military Construction' in value:
+                unique_grouped09.ix[i[0],'path'] = [110,'bill','house','hr2638']
+            else:
+                unique_grouped09.ix[i[0],'path'] = [111,'bill','house', 'hr1105']
+        
+        elif 'Report' in poc: 
+            rep = re.findall('\d+', value)[1]
+            if 'H. Report' in poc: 
+                path = [111,'report', 'house', rep]
+            else: 
+                path = [111,'report', 'senate', rep]
+            
+            unique_grouped09.ix[i[0],'path'] = path
+        
+        elif 'S. ' in poc and 'Report' not in poc: 
+            bi = 's'+str(re.findall('\d+', value)[0])
+            path = [111,'bill', 'senate', bi]
+            unique_grouped09.ix[i[0],'path'] = path
+
+
+
+unique_grouped09.to_csv("2009_pd.csv"
+
 earmarks09  = pd.read_csv("2009_pd.csv")
 
 earmarks09['doc_id'] = ' '
