3ee512fd1558cf4bb5c5464311cf956dcd2e0226
Madian Khabsa <madian@psu.edu>
2014-07-14 19:42:33 +0000

    positive example generation and querying search engines for wikipedia matches

--- src/python/classification/gernerate_positive_examples.py
+++ src/python/classification/gernerate_positive_examples.py
@@ -8,23 +8,26 @@ import csv
 import math
 import psycopg2
 import psycopg2.extras
-
+import random
+import time
+import codecs 
 from util import wiki_tools
 
 CONN_STRING = "dbname=harrislight user=harrislight password=harrislight host=dssgsummer2014postgres.c5faqozfo86k.us-west-2.rds.amazonaws.com"
 
 
-def get_positive_eamples(year):
+def get_positive_eamples(year, outfile):
 	entities =get_earmark_entities(year)
+	f = codecs.open(outfile,'w', 'utf8')
 	for entity in entities:
-		 pages = wiki_tools.get_wiki_page_title_google(entity['entity_inferred_name'])
-		 if len(pages)>1:
-		 		hierarchy = wiki_tools.get_category_hierarchy(pages[0], 3)
-		 		pprint(hierarchy)
-
-
-
-
+		pages = wiki_tools.get_wiki_page_title_google_cse(entity['entity_inferred_name'])
+		if len(pages)>1:
+		    #hierarchy = wiki_tools.get_category_hierarchy(pages[0], 1)
+		    f.write( "%d\t%s\n"%(entity["matched_entity_id"], pages[0]))
+		    #print "entity:" + entity['entity_inferred_name']
+		    #print "wikipedia page: "+ pages[0]
+	f.close()
+		 		
 
 def get_earmark_entities(year):
     """
@@ -34,7 +37,6 @@ def get_earmark_entities(year):
     columns = ["matched_entity_id", "entity_inferred_name"]
     cmd = "select "+", ".join(columns)+" from entities, earmark_documents, earmarks where \
      matched_entity_id = entities.id and earmarks.earmark_id = earmark_documents.earmark_id and enacted_year = %s"
-    print cmd
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(cmd, (year,))
     records = cur.fetchall()
@@ -50,5 +52,6 @@ def get_earmark_entities(year):
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='get positive examples')
     parser.add_argument('--year', type = int, required=True, help='year for pulling data')
+    parser.add_argument('--outfile',  required=True, help='the file to which to write results')
     args = parser.parse_args()
-    get_positive_eamples(args.year)
+    get_positive_eamples(args.year, args.outfile)
--- src/python/util/wiki_tools.py
+++ src/python/util/wiki_tools.py
@@ -2,11 +2,13 @@ import MySQLdb
 import sys
 import os
 import codecs
+import urllib
 import urllib2
 import json
 import requests
 import ConfigParser
 from urlparse import *
+import time
 
 db = MySQLdb.connect(host="sunlight.c5faqozfo86k.us-west-2.rds.amazonaws.com",user="sunlight", passwd="govtrack", db="wikipedia")
 
@@ -42,27 +44,75 @@ def get_categories(page_name, is_category=False):
     categories = [result[0] for result in cur.fetchall()]
     return categories
 
-def get_wiki_page_title_google(entity_name):
-    query = "%s+site:en.wikipedia.org" % entity_name.replace(" ", "+")
+def get_wiki_page_title_google(entity_name, max_tries = 5):
+    query = '%s site:en.wikipedia.org' % (entity_name)
+    query = urllib.quote(query)
     google_url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s" % query
-    r = requests.get(google_url)
-    results = r.json()["responseData"]["results"]
-    return [result["url"].split("/")[-1] for result in results]
+    wait_time = 3
+    for i in range(max_tries):
+        try:
+            print i
+            r = requests.get(google_url)
+            results = r.json()["responseData"]["results"]
+            if len(results) > 0:
+                return [result["url"].split("/")[-1] for result in results]
+            else:
+                return []
+        except Exception as ex:
+            time.sleep(wait_time)
+            wait_time = wait_time * 2
+            pass
+    print "Warning: max retries reached with query %s" %(entity_name)
+    return []
+    
+def get_wiki_page_title_google_cse(entity_name, max_tries = 5):
+    """
+    MAKE SURE TO DELETE THIS FUNCTION BEFORE OPEN SOURCING THE CODE
+    here no need to filter with wikipedia, cse already filters for wikipedia
+    """
+    pass
+    query = urllib.quote(entity_name)
+    google_url = "https://www.googleapis.com/customsearch/v1element?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=filtered_cse&num=10&hl=en&prettyPrint=false&source=gcsc&gss=.com&cx=016849338463251080306:gukdipzeymu&sort=&googlehost=www.google.com&oq=a&q=%s" % query
+    wait_time = 3
+    for i in range(max_tries):
+        try:
+            r = requests.get(google_url)
+            results = r.json()["results"]
+            if len(results) > 0:
+                return [result["url"].split("/")[-1] for result in results]
+            else:
+                return []
+        except Exception as ex:
+            print "error in try %d for query %s" %(i, entity_name),  ex
+            time.sleep(wait_time)
+            pass
+    print "Warning: max retries reached with query %s" %(entity_name)
+    return []
 
-def get_wiki_page_title_bing(entity_name):
+def get_wiki_page_title_bing(entity_name, max_tries = 5):
     bing_url = "https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web"
-    key = get_key("bing")
-    quoted_query = urllib2.quote("%s site:en.wikipedia.org" % entity_name)
+    key = "WGFuNGBlImIYGVdsYsCUHzDA3oX/4CEgYhuYOF0UrE0"#get_key("bing")
+    quoted_query = urllib2.quote('%s site:en.wikipedia.org' % entity_name)
     url = bing_url + r"?Query=%27" +quoted_query + r"%27&$format=json"
     password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
     password_mgr.add_password(None, url,key,key)
-    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-    opener = urllib2.build_opener(handler)
-    urllib2.install_opener(opener)
-    content = urllib2.urlopen(url).read()
-    j = json.loads(content)
-    user_ids = []
-    return [result["Url"].split("/")[-1] for result in j["d"]["results"]]
+    urls = []
+    for i in range(max_tries):
+        try:
+            handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+            opener = urllib2.build_opener(handler)
+            urllib2.install_opener(opener)
+            content = urllib2.urlopen(url).read()
+            j = json.loads(content)
+            user_ids = []
+            if j.has_key("d"):
+                if j["d"].has_key("results"):
+                    if len(j["d"]["results"]) > 0:
+                        urls= [result["Url"].split("/")[-1] for result in j["d"]["results"]]
+                        return urls
+        except Exception as ex:
+            pass
+    return urls            
 
 def get_key(key_type):
     cp = ConfigParser.ConfigParser()
