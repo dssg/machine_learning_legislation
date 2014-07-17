2e68eae7bf5f92dbe12ed35ee7feff13cd747909
OpsWorks user ewulczyn <ewulczyn@sunlight.localdomain>
2014-07-17 18:32:43 +0000

    starting diagnostics file

--- src/python/classification/diagnostics.py
+++ src/python/classification/diagnostics.py
@@ -1,6 +1,3 @@
-"""
-classification methods
-"""
 import os, sys, inspect
 sys.path.insert(0, os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],".."))))
 import argparse
@@ -21,59 +18,17 @@ from pipe import Pipe
 import pickle
 import marshal
 from sklearn import svm, grid_search
+import classifier
+from sklearn.cross_validation import StratifiedShuffleSplit
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-CONN_STRING = "dbname=harrislight user=harrislight password=harrislight host=dssgsummer2014postgres.c5faqozfo86k.us-west-2.rds.amazonaws.com"
 
-NO_WIKI_PAGE_FEATURE = "NO_WIKIPEDIA_PAGE_WAS_FOUND"
-
-
-    
-def read_entities_file(path):
-    """
-    reads a file containing entity ids
-    """
-    with open(path) as f:
-        return [int(line.strip()) for line in f.readlines() if len(line) > 0]
-        
-def get_entity_objects(entities):
-    return [Entity(eid) for eid in entities]
-    
-def get_instances_from_entities(entities, target_class):
-    return [Instance(entity, target_class) for entity in entities]
-
-def convert_to_svmlight_format(X, Y, entities, path):
-    """
-    converts x and y into svmlight representaiton and write's it to path
-    """
-    f = open(path,'w')
-    previous_row = -1
-    values =  zip(X.row, X.col, X.data)
-    rows = {}
-    for i, j, v in values:
-        if not rows.has_key(i):
-            rows[i] = []
-        rows[i].append((i,j,v))
-    
-    for i, list_values in rows.iteritems():
-        if Y[i]:
-            label = 1
-        else:
-            label = -1
-        f.write("%d " %(label))
-        for t in list_values:
-            f.write("%d:%d " %(t[1]+1, t[2]))
-        f.write("#%d\n" %(entities[i]))
-    f.close()
-        
 
 def classify_svm_cv(X, Y, folds=2):
     C = 1.0
-    #X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
     clf = svm.SVC(kernel='linear', C=C)
     #clf = sklearn.ensemble.RandomForestClassifier()
-    #s = clf.score(X_test, y_test)
     logging.info("Starting cross validation")
     skf = cross_validation.StratifiedKFold(Y, n_folds=folds)
     scores = cross_validation.cross_val_score(clf, X, Y, cv=skf, n_jobs=3)
@@ -82,17 +37,47 @@ def classify_svm_cv(X, Y, folds=2):
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
-def grid_search(X, Y, folds = 5):
+def do_grid_search(X, Y, folds = 5):
+
     param_grid = {'C': [0.1, 1, 10, 100, 1000], 'kernel': ['linear']}
     svr = svm.SVC()
     strat_cv = cross_validation.StratifiedKFold(Y, n_folds=folds)
-
-    clf = grid_search.GridSearchCV(cv  = strat_cv, estimator = svr, param_grid  =parameters)
+    clf = grid_search.GridSearchCV(cv  = strat_cv, estimator = svr, param_grid  =param_grid, scoring = 'f1')
     clf.fit(X, Y)
-    print clf
+
+    print("Grid scores on development set:")
+    for params, mean_score, scores in clf.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() / 2, params))
+
+    print "\n"
+
+
+
+
+    
+    print("Best parameters set found on development set:")
+    best_parameters, score, _ = max(gs_clf.grid_scores_, key=lambda x: x[1])
+    print best_parameters
+
+    
+
+
+    print("Detailed classification report:")
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
+
+
+
+
+
 
 def serialize_instances(instances, outfile):
-    marshal.dump(instances, open(outfile,'wb'))
+    pickle.dump(instances, open(outfile,'wb'))
     
 def main():
     parser = argparse.ArgumentParser(description='build classifier')
@@ -104,61 +89,54 @@ def main():
 
 
     parser_grid = subparsers.add_parser('grid', help='perform CV grid search')
-    parser_grid.add_argument('--folds', type=int, required=True, help='number of folds')
+    parser_grid.add_argument('--folds', type=int, required=False, help='number of folds')
     parser_grid.add_argument('--file',  required=True, help='file to pickled instances')
-    
-    parser_transform = subparsers.add_parser('transform', help='transform to svmlight format')
-    parser_transform.add_argument('--outfile', required=True, help='path to output file')
     
     parser_pickle = subparsers.add_parser('serialize', help='pickle instances')
     parser_pickle.add_argument('--outfile', required=True, help='path to output pickled file')
-
     parser_pickle.add_argument('--positivefile', required=True, help='file containing entities identified as earmarks')
     parser_pickle.add_argument('--negativefile',  required=True, help='file containing negative example entities')
-    #parser.add_argument('--depth', type=int, default = 3,  help='wikipedia category level depth')
-    #parser.add_argument('--ignore_levels', action='store_false', default=False, help='distinguish between category levels')
-    
     args = parser.parse_args()
-    #distinguish_levels = not args.ignore_levels
-    
-    
-    
+
+    print "parsed"
     
     
     #x, y, space = encode_instances(positive_entities, negative_entities, args.depth, distinguish_levels)
     
     if args.subparser_name =="cv":
         logging.info("Start deserializing")
-        pipe = Pipe( instances= marshal.load(open(args.file, 'rb')))
-        logging.info("Start creating X, Y")
-        x,y,space = pipe.instances_to_scipy_sparse() 
+        #pipe = Pipe( instances= marshal.load(open(args.file, 'rb')))
+        logging.info("Start loading X, Y")
+        #x,y,space = pipe.instances_to_scipy_sparse() 
+        (x,y) = pickle.load(open(args.file, 'rb'))
         classify_svm_cv(x, y, args.folds)
 
 
     elif args.subparser_name =="grid":
         logging.info("Start deserializing")
-        pipe = Pipe( instances= marshal.load(open(args.file, 'rb')))
-        logging.info("Start creating X, Y")
-        x,y,space = pipe.instances_to_scipy_sparse() 
-        grid_search(x, y, args.folds)
+        #pipe = Pipe( instances= marshal.load(open(args.file, 'rb')))
+        logging.info("Start loading X, Y")
+        #x,y,space = pipe.instances_to_scipy_sparse() 
+        (x,y) = pickle.load(open(args.file, 'rb'))
+        do_grid_search(x, y, args.folds)
  
         
-    elif args.subparser_name == "transform":
-        convert_to_svmlight_format(x, y, positive_entities+negative_entities, args.outfile)
-        
     elif args.subparser_name == "serialize":
-        positive_entities = read_entities_file(args.positivefile)
-        negative_entities = read_entities_file(args.negativefile)
+        positive_entities = classifier.read_entities_file(args.positivefile)
+        negative_entities = classifier.read_entities_file(args.negativefile)
+        print "read entities"
         logging.info("Pulling entities from database")
-        positive_instance = get_instances_from_entities(get_entity_objects(positive_entities), 1 )
-        negative_instance = get_instances_from_entities(get_entity_objects(negative_entities), 0 )
+        positive_instance = classifier.get_instances_from_entities(classifier.get_entity_objects(positive_entities), 1 )
+        negative_instance = classifier.get_instances_from_entities(classifier.get_entity_objects(negative_entities), 0 )
         instances = positive_instance + negative_instance
         logging.info("Creating pipe")
         pipe = Pipe([wikipedia_categories_feature_generator.wikipedia_categories_feature_generator(depth = 3,distinguish_levels=True ),], 
         instances)
         logging.info("Pushing into pipe")
         pipe.push_all()
-        serialize_instances(instances, args.outfile)
-        
+        x,y,space = pipe.instances_to_scipy_sparse() 
+        serialize_instances((x,y), args.outfile)
+
+
 if __name__=="__main__":
     main()
