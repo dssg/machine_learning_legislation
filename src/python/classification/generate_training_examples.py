5e2b1aa215477bd9d221a908d982d76c34fadd9d
Madian Khabsa <madian@psu.edu>
2014-07-21 18:26:36 +0000

    separating instance generation from matching to google

--- src/python/classification/generate_training_examples.py
+++ src/python/classification/generate_training_examples.py
@@ -13,6 +13,8 @@ import time
 import codecs 
 from util import wiki_tools
 import logging
+from dao import Entity
+from data_importer import import_entity_wikipedia_mapping
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -20,24 +22,26 @@ CONN_STRING = "dbname=harrislight user=harrislight password=harrislight host=dss
 
 
 def get_positive_eamples(year, outfile):
-	entities =get_earmark_entities(year)
-	f = codecs.open(outfile,'w', 'utf8')
-	for entity in entities:
-		pages = wiki_tools.get_wiki_page_title_google_cse(entity['entity_inferred_name'])
-		if len(pages)>1:
-		    f.write( "%d\t%s\n"%(entity["matched_entity_id"], pages[0]))
-	f.close()
-	
+	entity_ids = [entity["matched_entity_id"] for entity in  get_earmark_entities(year)]
+	write_entity_ids_to_file(entity_ids, outfile)
 
-def get_negative_examples(congress, count, outfile):
-    entities =get_candiadte_negative_examples_from_db(congress, count)
-    f = codecs.open(outfile,'w', 'utf8')
-    for entity in entities:
-		pages = wiki_tools.get_wiki_page_title_google_cse(entity['entity_inferred_name'])
-		if len(pages)>1:
-		    f.write( "%d\t%s\n"%(entity["mid"], pages[0]))
+def write_entity_ids_to_file(entities, path):
+    f = codecs.open(path,'w', 'utf8')
+    for entity_id in entities:
+		f.write( "%d\n"%(entity_id))
     f.close()
+    	
+def get_negative_examples(congress, count, outfile):
+    entity_ids = [entity["mid"] for entity in  get_candiadte_negative_examples_from_db(congress, count)]
+    write_entity_ids_to_file(entity_ids, outfile)
+    
 
+def match_entities_to_google(entity_ids):
+    for eid in entity_ids:
+        entity = Entity.Entity(eid)
+        pages = wiki_tools.get_wiki_page_title_google_cse(entity.entity_inferred_name)
+        if len(pages)>1:
+            import_entity_wikipedia_mapping.import_mapping( [(eid, pages[0]),] )
 
 def get_earmark_entities(year):
     """
@@ -98,8 +102,18 @@ if __name__=="__main__":
     parser_negative.add_argument('--count', type = int, required=True, help='number of negative examples')
     parser_negative.add_argument('--outfile',  required=True, help='the file to which to write results')
     
+    parser_google = subparsers.add_parser('google', help='match entities in file to wikipedia')
+    parser_google.add_argument('--file',  required=True, help='the file containing list of entity ids')
+    
     args = parser.parse_args()
     if args.subparser_name =="positive":
         get_positive_eamples(args.year, args.outfile)
-    else:
+    elif args.subparser_name =="negative":
         get_negative_examples(args.congress, args.count, args.outfile)
+    elif args.subparser_name =="google":
+        entity_ids = [int(line.strip()) for line in open(args.file).readlines() if len(line) > 1 ]
+        match_entities_to_google(entity_ids)
+        
+        
+    
+    
