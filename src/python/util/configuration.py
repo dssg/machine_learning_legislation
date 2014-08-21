ec9e5f4d52ead9cec8299775a454e2010a9614d6
Matthew Heston <matthewheston@gmail.com>
2014-08-21 00:02:26 +0000

    Add configuration methods to get paths to data folders.

--- src/python/util/configuration.py
+++ src/python/util/configuration.py
@@ -13,3 +13,18 @@ def get_connection_string():
 
 def get_data_folder():
     return config.get("keys","data")
+
+def get_path_to_reports():
+    return os.path.join(get_data_folder(), "congress_reports")
+
+def get_path_to_bills():
+    return os.path.join(get_data_folder(), "bills")
+
+def get_path_to_classification_data():
+    return os.path.join(get_data_folder(), "classification_data")
+
+def get_path_to_omb_data():
+    return os.path.join(get_data_folder(), "OMB")
+
+def get_path_to_plaws():
+    return os.path.join(get_data_folder(), "plaws")
